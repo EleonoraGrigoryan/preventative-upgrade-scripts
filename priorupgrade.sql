ce6a543d5dee5e6bef8647d72c459b8c6965733f
Eleonora Grigoryan <eleonora.grigoryan@outlook.com>
2018-08-23 18:14:17 +0400

    Update priorupgrade.sql

--- priorupgrade.sql
+++ priorupgrade.sql
@@ -1,25 +1,6 @@
--- Prior to Upgrade
+-- Pre-upgrade tasks
 
--- disable all startup sprocs
-
-declare cr_startup cursor
-for
-select name from sys.procedures where objectproperty([object_id],'ExecIsStartUp') = 1
-
-open cr_startup
-declare @var sysname
-fetch next from cr_startup into @var
-
-while @@fetch_status = 0
-begin
-	exec sp_procoption @var, 'startup', 'off'
-	fetch next from cr_startup into @var
-end
-close cr_startup
-deallocate cr_startup
-
-
--- disable all enabled trace flag functionalities
+-- disable the functionalities of all enabled trace flags 
 
 if object_id('tempdb..#tb1', 'U') is not null 
 drop table #tb1
@@ -85,4 +66,22 @@ end
 close cr_dbnames
 deallocate cr_dbnames
 
+
+-- disable all startup sprocs
+
+declare cr_startup cursor
+for
+select name from sys.procedures where objectproperty([object_id],'ExecIsStartUp') = 1
+
+open cr_startup
+declare @var sysname
+fetch next from cr_startup into @var
+
+while @@fetch_status = 0
+begin
+	exec sp_procoption @var, 'startup', 'off'
+	fetch next from cr_startup into @var
+end
+close cr_startup
+deallocate cr_startup
 
