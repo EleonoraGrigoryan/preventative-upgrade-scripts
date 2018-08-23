e594b25f0c65f011c5b52feaffacd6c2b56ec237
Eleonora Grigoryan <eleonora.grigoryan@outlook.com>
2018-08-23 15:10:29 +0400

    disable all enabled trace flag functionalities

--- priorupgrade.sql
+++ priorupgrade.sql
@@ -1,4 +1,4 @@
---disable all startup sprocs
+-- disable all startup sprocs
 
 declare cr_startup cursor
 for
@@ -16,3 +16,28 @@ end
 close cr_startup
 deallocate cr_startup
 
+-- disable all enabled trace flag functionalities
+
+if object_id('tempdb..#tb1', 'U') is not null 
+drop table #tb1
+
+create table #tb1 ([name] int, [status] int, [global] int, [session] int)
+insert into #tb1 exec('dbcc tracestatus()')
+
+declare cr_traceflagsoff cursor 
+for
+select [name] from #tb1
+
+open cr_traceflagsoff 
+declare @trfl int
+fetch next from cr_traceflagsoff into @trfl
+
+while @@fetch_status = 0
+begin
+	dbcc traceoff(@trfl, -1)
+	fetch next from cr_traceflagsoff into @trfl
+end
+
+close cr_traceflagsoff
+deallocate cr_traceflagsoff
+-- dbcc tracestatus()
